b2080bc72aa7fb3eae7d9bb17f640fa6434a1687
andv@paditech.org <andv@paditech.org>
2020-11-16 09:42:56 +0700

    'pull-code'

--- website/app/Providers/RouteServiceProvider.php
+++ website/app/Providers/RouteServiceProvider.php
@@ -47,7 +47,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
 
-            Route::prefix('admin-fail')
+            Route::prefix('admin')
                 ->middleware('auth')
                 ->namespace('App\Http\Controllers\Admin')
                 ->group(base_path('routes/admin.php'));
